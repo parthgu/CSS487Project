ed20e50ce0823f368e6562ec32f0fd55dc141f2a
parthgu <gparth02@hotmail.com>
2023-12-05 19:32:48 -0800

    added filters for smile

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main(int argc, const char *argv[])
 	Mat img;
 	Mat imgGray;
 	Mat teardrop = imread("teardrop.png", IMREAD_UNCHANGED);
+	Mat rainbowsmile = imread("rainbowsmile.png", IMREAD_UNCHANGED);
 
 	string haarCascadePath = "haarcascade_frontalface_default.xml";
 	string smileCascadePath = "haarcascade_smile.xml";
@@ -74,35 +75,38 @@ int main(int argc, const char *argv[])
 			eyeCascade.detectMultiScale(faceROI, eyes, 1.1, 20, 0 | CASCADE_SCALE_IMAGE, Size(5, 5));
 
 			vector<Rect> smiles;
-			smileCascade.detectMultiScale(faceROI, smiles, 1.1, 15, 0 | CASCADE_SCALE_IMAGE, Size(80, 80));
+			smileCascade.detectMultiScale(faceROI, smiles, 1.1, 50, 0 | CASCADE_SCALE_IMAGE, Size(80, 80));
 
 			for (Rect &eye : eyes)
 			{
 				rectangle(img, face.tl() + eye.tl(), face.tl() + eye.br(), Scalar(255, 50, 50), 2);
 				putText(img, "Eye", face.tl() + eye.tl() - Point2i(-2, 5), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 50, 50), 2);
 
-				// Resize teardrop image to fit the size of the detected eye
-				Mat resizedTeardrop;
-				resize(teardrop, resizedTeardrop, eye.size());
-
-				// Get the region of interest (ROI) for the teardrop
-				Mat teardropROI(resizedTeardrop.rows, resizedTeardrop.cols, CV_8UC4, Scalar(0, 0, 0, 0));
-				resizedTeardrop(Rect(0, 0, resizedTeardrop.cols, resizedTeardrop.rows)).copyTo(teardropROI);
-
-				// Calculate the position to overlay the teardrop
-				Point teardropPosition = face.tl() + eye.tl() + Point(0, eye.height / 1.5);
-
-				// Overlay the teardrop onto the original image
-				for (int y = 0; y < teardropROI.rows; ++y)
+				if (smiles.size() <= 0)
 				{
-					for (int x = 0; x < teardropROI.cols; ++x)
+					// Resize teardrop image to fit the size of the detected eye
+					Mat resizedTeardrop;
+					resize(teardrop, resizedTeardrop, eye.size());
+
+					// Get the region of interest (ROI) for the teardrop
+					Mat teardropROI(resizedTeardrop.rows, resizedTeardrop.cols, CV_8UC4, Scalar(0, 0, 0, 0));
+					resizedTeardrop(Rect(0, 0, resizedTeardrop.cols, resizedTeardrop.rows)).copyTo(teardropROI);
+
+					// Calculate the position to overlay the teardrop
+					Point teardropPosition = face.tl() + eye.tl() + Point(0, eye.height / 1.5);
+
+					// Overlay the teardrop onto the original image
+					for (int y = 0; y < teardropROI.rows; ++y)
 					{
-						Vec4b teardropPixel = teardropROI.at<Vec4b>(y, x);
-						if (teardropPixel[3] > 0) // Check the alpha channel
+						for (int x = 0; x < teardropROI.cols; ++x)
 						{
-							img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[0] = teardropPixel[0];
-							img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[1] = teardropPixel[1];
-							img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[2] = teardropPixel[2];
+							Vec4b teardropPixel = teardropROI.at<Vec4b>(y, x);
+							if (teardropPixel[3] > 0) // Check the alpha channel
+							{
+								img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[0] = teardropPixel[0];
+								img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[1] = teardropPixel[1];
+								img.at<Vec3b>(teardropPosition.y + y, teardropPosition.x + x)[2] = teardropPixel[2];
+							}
 						}
 					}
 				}
@@ -112,6 +116,32 @@ int main(int argc, const char *argv[])
 			{
 				rectangle(img, face.tl() + smile.tl(), face.tl() + smile.br(), Scalar(50, 255, 50), 2);
 				putText(img, "Smile", face.tl() + smile.tl() - Point2i(-2, 5), FONT_HERSHEY_SIMPLEX, 1, Scalar(50, 255, 50), 2);
+
+				// Resize rainbowsmile image to fit the width of the detected smile
+				int resizedRainbowsmileWidth = smile.width;
+				int resizedRainbowsmileHeight = static_cast<int>((static_cast<float>(resizedRainbowsmileWidth) / rainbowsmile.cols) * rainbowsmile.rows);
+
+				Mat resizedRainbowsmile;
+				resize(rainbowsmile, resizedRainbowsmile, Size(resizedRainbowsmileWidth, resizedRainbowsmileHeight));
+
+				// Calculate the position to overlay the rainbowsmile at the center of the mouth
+				Point rainbowsmilePosition = face.tl() + smile.tl() + Point(smile.width / 2 - resizedRainbowsmile.cols / 2, (smile.height / 2 - resizedRainbowsmile.rows / 2) / 1.25);
+
+				// Overlay the rainbowsmile onto the original image with transparency
+				for (int y = 0; y < resizedRainbowsmile.rows; ++y)
+				{
+					for (int x = 0; x < resizedRainbowsmile.cols; ++x)
+					{
+						Vec4b rainbowsmilePixel = resizedRainbowsmile.at<Vec4b>(y, x);
+						if (rainbowsmilePixel[3] > 0) // Check the alpha channel
+						{
+							Vec3b &imgPixel = img.at<Vec3b>(rainbowsmilePosition.y + y, rainbowsmilePosition.x + x);
+							imgPixel[0] = static_cast<uchar>((rainbowsmilePixel[3] / 255.0) * rainbowsmilePixel[0] + (1.0 - rainbowsmilePixel[3] / 255.0) * imgPixel[0]);
+							imgPixel[1] = static_cast<uchar>((rainbowsmilePixel[3] / 255.0) * rainbowsmilePixel[1] + (1.0 - rainbowsmilePixel[3] / 255.0) * imgPixel[1]);
+							imgPixel[2] = static_cast<uchar>((rainbowsmilePixel[3] / 255.0) * rainbowsmilePixel[2] + (1.0 - rainbowsmilePixel[3] / 255.0) * imgPixel[2]);
+						}
+					}
+				}
 			}
 		}
 
